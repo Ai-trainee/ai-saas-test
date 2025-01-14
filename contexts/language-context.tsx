242a058dbb0293f1145226c72faafe0922af7d1a
Aitrainee <2545197649@qq.com>
2025-01-14 08:18:01 +0000

    就是说他没有记住用户的切换，虽然不进行浏览器刷新。是可以记住没有问题的

--- contexts/language-context.tsx
+++ contexts/language-context.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 
 type Language = 'en' | 'zh'
 
@@ -14,8 +14,20 @@ const LanguageContext = createContext<LanguageContextType | undefined>(undefined
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
     const [language, setLanguage] = useState<Language>('en')
 
+    useEffect(() => {
+        const savedLanguage = localStorage.getItem('language') as Language
+        if (savedLanguage) {
+            setLanguage(savedLanguage)
+        }
+    }, [])
+
+    const handleSetLanguage = (lang: Language) => {
+        setLanguage(lang)
+        localStorage.setItem('language', lang)
+    }
+
     return (
-        <LanguageContext.Provider value={{ language, setLanguage }}>
+        <LanguageContext.Provider value={{ language, setLanguage: handleSetLanguage }}>
             {children}
         </LanguageContext.Provider>
     )
